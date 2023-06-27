01ef189b7bbebd7e6dbd89e23eb481ba9fee9927
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-27 19:31:32 +0200

    generated 1 email

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`
+
+const app = Vue.createApp({
+    data(){
+        return{
+            emails:[],
+        }
+    },
+    created(){
+        axios.get(`${endpoint}`)
+        .then((res) => {
+            const responseEmails = res.data.response;
+            this.emails.push(responseEmails);
+        })
+    },
+})
+
+app.mount(`#root`);
